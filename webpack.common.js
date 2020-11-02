09282d5191debe4784726e73d3c09758ac4fa308
Andrii <costivandrei@gmail.com>
2020-11-02 22:34:49 +0000

    fixed webpack config

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+module.exports = {
+  entry: {
+    main: "./src/main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ]
+  }
+};
