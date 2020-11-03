a6f63c7b5bfecb09fef53e9710c3bc5526051479
Andrii <costivandrei@gmail.com>
2020-11-03 14:13:16 +0000

    added images to list

--- src/functionality/SetUpDraggable.js
+++ src/functionality/SetUpDraggable.js
@@ -8,10 +8,19 @@ import {
 } from './DragFunctions';
 
 let Dragable = document.getElementsByClassName('draggable');
+let DragIcon = document.getElementsByClassName('dragIcon');
 
 const SetUpDraggable = () => {
+    for (let key in DragIcon){
+        if (key.length === 1) {
+            DragIcon[key].addEventListener('mousedown', function() { this.parentNode.setAttribute("draggable", false); });
+            DragIcon[key].addEventListener('mouseup', function() { this.parentNode.setAttribute("draggable", true); });
+        }
+    }
+    
     for (let key in Dragable){
         if (key.length === 1) {
+                    
             Dragable[key].addEventListener('dragstart', dragStart, false);
             Dragable[key].addEventListener('dragenter', dragEnter, false);
             Dragable[key].addEventListener('dragover', dragOver, false);
