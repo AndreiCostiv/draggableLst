3d824650cbdbad9b5eaf3648e4e3d420c411c0fb
Andrii <costivandrei@gmail.com>
2021-01-24 21:23:16 +0000

    backup

--- src/functionality/SortableJS.js
+++ src/functionality/SortableJS.js
@@ -3,6 +3,7 @@ import Sortable from "sortablejs";
 let SortableList = document.getElementsByClassName('sortableList')[0];
 
 let SortableJS = () => {    
+    console.log(SortableList);
     new Sortable(SortableList, {
         handle: '.dragIcon', // handle's class
         animation: 150
